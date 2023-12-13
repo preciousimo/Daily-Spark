5f5a7d58358c57a2a55ece5f623a2d495154daee
preciousimo <preciousimoniakemu@gmail.com>
2023-12-13 16:49:04 +0100

    Advent calender list

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = [...Array(30)].map((val, index) => index + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({item}) => <DayListItem day={item} />}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +25,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+
+  content: {
+    gap: 10,
+  },
+
+  column: {
+    gap: 10,
+    padding: 10,
+  },
+
+  box: {
+    backgroundColor: '#F9EDE3',
+    flex: 1,
+    aspectRatio: 1,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '9b4521',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  text: {
+    color: '#9b4521',
+    fontSize: 70,
+  }
 });
