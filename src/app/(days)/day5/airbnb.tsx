231a6337f53d512bf7338228c2ac0878bcbf3a7b
preciousimo <preciousimoniakemu@gmail.com>
2024-01-05 09:48:21 +0100

    Implement custom markers

--- src/app/(days)/day5/airbnb.tsx
+++ src/app/(days)/day5/airbnb.tsx
@@ -2,30 +2,33 @@ import React from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { Stack } from 'expo-router';
+import apartments from '@assets/data/day5/apartments.json'
 
 export default function AirbnbScreen() {
+    // Calculate the average latitude and longitude for initial region
+  const averageLatitude = apartments.reduce((sum, apt) => sum + apt.latitude, 0) / apartments.length;
+  const averageLongitude = apartments.reduce((sum, apt) => sum + apt.longitude, 0) / apartments.length;
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ headerShown: false }} />
       <MapView
         style={styles.map}
         initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
+          latitude: averageLatitude,
+          longitude: averageLongitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       >
-        <Marker
-            coordinate={{latitude: 37.78825, longitude: -122.4324,}}
-            title='2bd Apartment'
-            description='Hello!'
-        />
-        <Marker
-            coordinate={{latitude: 37.78824, longitude: -122.4323,}}
-            title='1bd Apartment'
-            description='Hello there!'
-        />
+        {apartments.map((apartment) => (
+             <Marker
+             key={apartment.id.toString()}
+             coordinate={{latitude: apartment.latitude, longitude: apartment.longitude,}}
+             title={apartment.title}
+             description='Hello!'
+            />
+        ))}
       </MapView>
     </View>
   );
