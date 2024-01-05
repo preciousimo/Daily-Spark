8c5b709bf8bc5f08039211c72886fd9dfaf68931
preciousimo <preciousimoniakemu@gmail.com>
2024-01-05 11:17:19 +0100

    Customize marker view and add styling

--- src/app/(days)/day5/airbnb.tsx
+++ src/app/(days)/day5/airbnb.tsx
@@ -1,45 +1,61 @@
 import React from 'react';
-import MapView, {Marker} from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
 import { Stack } from 'expo-router';
 import apartments from '@assets/data/day5/apartments.json'
 
 export default function AirbnbScreen() {
     // Calculate the average latitude and longitude for initial region
-  const averageLatitude = apartments.reduce((sum, apt) => sum + apt.latitude, 0) / apartments.length;
-  const averageLongitude = apartments.reduce((sum, apt) => sum + apt.longitude, 0) / apartments.length;
+    const averageLatitude = apartments.reduce((sum, apt) => sum + apt.latitude, 0) / apartments.length;
+    const averageLongitude = apartments.reduce((sum, apt) => sum + apt.longitude, 0) / apartments.length;
 
-  return (
-    <View style={styles.container}>
-      <Stack.Screen options={{ headerShown: false }} />
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: averageLatitude,
-          longitude: averageLongitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        {apartments.map((apartment) => (
-             <Marker
-             key={apartment.id.toString()}
-             coordinate={{latitude: apartment.latitude, longitude: apartment.longitude,}}
-             title={apartment.title}
-             description='Hello!'
-            />
-        ))}
-      </MapView>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Stack.Screen options={{ headerShown: false }} />
+            <MapView
+                provider={PROVIDER_GOOGLE}
+                style={styles.map}
+                initialRegion={{
+                    latitude: averageLatitude,
+                    longitude: averageLongitude,
+                    latitudeDelta: 0.1,
+                    longitudeDelta: 0.1,
+                }}
+            >
+                {apartments.map((apartment) => (
+                    <Marker
+                        key={apartment.id.toString()}
+                        coordinate={{ latitude: apartment.latitude, longitude: apartment.longitude, }}
+                        title={apartment.title}
+                        description='Hello!'
+                    >
+                        <View 
+                            style={{
+                                backgroundColor: 'wwhite',
+                                padding: 5,
+                                paddingHorizontal: 10,
+                                borderWidth: 1,
+                                borderColor: 'gray',
+                                borderRadius: 20,
+                            }}
+                        >
+                            <Text style={{fontFamily: 'InterBold'}}>
+                                ₦ {apartment.price}
+                            </Text>
+                        </View>
+                    </Marker>
+                ))}
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
 });
