7353db3d5c7045aaf7fb3279884a7f066b6b3955
preciousimo <preciousimoniakemu@gmail.com>
2024-01-06 09:15:05 +0100

    Add bottom sheet for list view of apartments

--- src/app/(days)/day5/airbnb.tsx
+++ src/app/(days)/day5/airbnb.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
 import { Stack } from 'expo-router';
 import apartments from '@assets/data/day5/apartments.json';
 import CustomMarker from '@/components/core/day5/CustomMarker';
 import ApartmentListItem from '@/components/core/day5/ApartmentListItem';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 export default function AirbnbScreen() {
     // Calculate the average latitude and longitude for the initial region
@@ -12,6 +13,9 @@ export default function AirbnbScreen() {
     const averageLongitude = apartments.reduce((sum, apt) => sum + apt.longitude, 0) / apartments.length;
 
     const [selectedApartment, setSelectedApartment] = useState(null);
+
+    // variables
+    const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
 
     return (
         <View style={styles.container}>
@@ -37,6 +41,17 @@ export default function AirbnbScreen() {
 
             {/* Display selected apartment */}
             {selectedApartment && <ApartmentListItem apartment={selectedApartment} />}
+
+            <BottomSheet
+                // ref={bottomSheetRef}
+                index={1}
+                snapPoints={snapPoints}
+                // onChange={handleSheetChanges}
+            >
+                <View style={styles.contentContainer}>
+                    <Text>Awesome 🎉</Text>
+                </View>
+            </BottomSheet>
         </View>
     );
 }
